4d87ea6d69a1aa4fa12564a47372c908c4877234
Julesljn <juleslejeunepro@gmail.com>
2024-10-02 09:52:43 +0200

    feat : romanToInteger Python

---
+++ romanToInteger.py
@@ -0,0 +1,13 @@
+class Solution:
+    NUMBER = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    def romanToInt(self, s: str) -> int:
+        pass  
